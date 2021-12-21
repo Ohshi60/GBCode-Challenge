22a44b4ce98aa75676753cf7da04534c464bc8b3
Michael Sebastian Søby <ohshi@Michaels-MacBook-Pro.local>
2021-12-21 20:15:49 +0100

    setup userRouter in backend

--- user-backend/server.js
+++ user-backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 
 const app = express()
+const userRouter = require('./routes/userRouter')
+
+//refactor into using dotenv environment variables at some point for port etc
+
+const PORT = 3001 
 
 
+app.use('/user', userRouter)
 
 app.get('/', (req,res) => {
   res.send('Welcome to root')
 })
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`)
 })
