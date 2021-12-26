3a98478b4e4388cc908bc37468f199a4403bbfe9
Michael Sebastian Søby <ohshi@Michaels-MBP.station>
2021-12-26 18:44:16 +0000

    WIP

--- user-backend/server.ts
+++ user-backend/server.ts
@@ -1,12 +1,21 @@
+// Library imports
 import express, {Request, Response} from 'express'
+import { initializeApp } from 'firebase/app';
+import {getAuth} from 'firebase/auth'
 
 const app = express();
-import userRouter from './routes/userRouter';
 
+//Custom Module imports
+import userRouter from './routes/userRouter';
+import firebaseConfig from './fb-config'
 //refactor into using dotenv environment variables at some point for port etc
+
 
 const PORT = 3001 
 
+//App setup
+const fbApp = initializeApp(firebaseConfig)
+const auth = getAuth(fbApp)
 
 app.use('/user', userRouter)
 
@@ -21,3 +30,5 @@ app.listen(PORT, () => {
 Implement from 
 https://console.firebase.google.com/u/0/project/very-sirius-proyecta/overview
 */
+
+
