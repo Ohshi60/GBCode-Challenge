2d50a2c966484cdbc1458ddc201aa5dc090395ed
Michael Sebastian Søby <ohshi@Michaels-MacBook-Pro.local>
2021-12-29 14:43:48 +0100

    clean a bit up in backend

--- user-backend/routes/userRouter.ts
+++ user-backend/routes/userRouter.ts
@@ -1,4 +1,4 @@
-import express = require('express')
+import express from 'express'
 
 const userRouter = express.Router()
 
@@ -6,21 +6,22 @@ const userRouter = express.Router()
 userRouter.post('/signup', (req,res) => {
 
     console.log('Signup user with form data', req.body)
-    res.send('user signed up')
+    console.log('email', req.body.email)
+    console.log('plaintext password', req.body.password)
+    res.send('user signed up')  
 })
 //sign in
 userRouter.post('/signin', (req,res) => {
-
     console.log('Posted data to the signin endpoint')
     res.send('signed in?')
 })
 
-//request user profile page
+//request user profile page - needs to be authenticated to access this
 userRouter.get('/:id', (req,res) => {
+    //if req.params.id === decodedToken.uid;
     console.log('received request frm ' , req)
     console.log('user profile for id' , req.params.id)
     res.send(`Welcome mr ${req.params.id}`)
-
-} )
+})
 
 export default userRouter
--- user-backend/server.ts
+++ user-backend/server.ts
@@ -1,25 +1,22 @@
 // Library imports
-import express, {Request, Response} from 'express'
-import { initializeApp } from 'firebase/app';
-import {getAuth} from 'firebase/auth'
+import express from 'express'
 import cors from 'cors'
+
 //Custom Module imports
 import userRouter from './routes/userRouter';
-import firebaseConfig from './fb-config'
+
 //Constants
-const PORT = 3001 
+const PORT = 3001;
+
 //App setup
 const app = express();
-const fbApp = initializeApp(firebaseConfig)
-const auth = getAuth(fbApp)
+
 //apply middleware
 app.use(cors())
 app.use(express.json())
 app.use('/user', userRouter)
 
-app.get('/', (req: Request,res: Response) => {
-  res.send('Welcome to root')
-})
+
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`)
 })
