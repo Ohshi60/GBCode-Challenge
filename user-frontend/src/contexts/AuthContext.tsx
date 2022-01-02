40ca05e8d2f353f79e93a1b09c030be7669532fb
Michael Sebastian Søby <mssoeby60@gmail.com>
2022-01-02 17:25:22 +0100

    refactor to use auth context - setup auth context

--- user-frontend/src/contexts/AuthContext.tsx
+++ user-frontend/src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { userInfo } from 'os';
-import React, {useContext, useState, createContext} from 'react'
-
+import React, {useContext, useState, createContext, useEffect} from 'react'
+import { auth } from '../services/auth';
 const defaultState = {
   currentUser: null
 }
@@ -10,17 +10,27 @@ const AuthContext = createContext({});
 export function useAuth() {
   useContext(AuthContext)
 }
-// eslint-disable-next-line
-export const AuthProvider  = ({children}) : React.PropsWithChildren<{}> => {
+
+export const AuthProvider: React.FC = ({children})  => {
  
-  const [currentUser, setCurrentUser] = useState()
+  const [currentUser, setCurrentUser] = useState<any | null>()
+  const [loading, setLoading] = useState(true)
+
   const value = {
     currentUser
   }
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged( user => {
+      setCurrentUser(user);
+      setLoading(false);
+    })
+    return unsubscribe
+  }, [])
+
   return (    
     <AuthContext.Provider value={value}>
-      {children}
+      {!loading && children}
     </AuthContext.Provider>
   ) 
 }
